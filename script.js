c4325fdeb804bba481c7fd64889b2e7079f79457
Hassan-shah9 <hassan.thepearlsystem@gmail.com>
2024-12-07 18:07:44 +0700

    working on script

--- script.js
+++ script.js
@@ -5,4 +5,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const easyProgressCircle = document.querySelector('.easy-progress');
     const mediumProgressCircle = document.querySelector('.medium-progress');
     const hardProgressCircle = document.querySelector('.hard-progress');
+    const easyLable = document.getElementById('easy-lable');
+    const mediumLable = document.getElementById('medium-lable');
+    const hardLable = document.getElementById('hard-lable');
+    const cardStatsContainer = document.querySelector('.stats-cards');
+
+    // return true or false based on whether
+    function validateUsername(username) {
+        if (username.trim() === '') {
+            alert('Please enter a username');
+            return false;
+        }
+        const regex = /^[a-zA-Z0-9_-]{1,16}$/;
+
+        const isMatching = regex.test(username);
+        if (!isMatching) {
+            alert('Invalid username');
+        }
+        return isMatching;
+    }
+
+    searchButton.addEventListener('click', function () {
+        const username = usernameInput.value;
+        console.log('logging username:', username);
+        if (validateUsername(username)) {
+            fetch();
+        }
+    });
 });
