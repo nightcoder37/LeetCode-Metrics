c66f37c6e6dfcb2e289790b6de90f4afa04ff61b
Hassan-shah9 <hassan.thepearlsystem@gmail.com>
2024-12-07 14:56:26 +0700

    css and javascript continue

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchButton = document.getElementById('search-btn');
+    const usernameInput = document.getElementById('user-input');
+    const statsContainer = document.querySelector('.stats-container');
+    const easyProgressCircle = document.querySelector('.easy-progress');
+    const mediumProgressCircle = document.querySelector('.medium-progress');
+    const hardProgressCircle = document.querySelector('.hard-progress');
+});
